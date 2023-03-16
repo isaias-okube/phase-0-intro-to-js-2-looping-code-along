71bdf58e7ee75ed14535756646313e13cca94f65
ISAIAS <issuyep@yahoo.com>
2023-03-16 00:40:46 -0600

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const messegesNames = [];
+
+function writeCards( names, value ) {
+    let messegesNames = []
+    for ( let i = 0;  i < names.length;  i++ ) {
+       messegesNames.push( `Thank you, ${names[i]}, for the wonderful ${value} gift!` );
+    }
+       return messegesNames;
+}
+function countDown(number) {
+    while (number >= 0) {
+        console.log (number)
+        number --
+    }
+}
+
+
+
+
